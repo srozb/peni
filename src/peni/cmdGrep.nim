f4d2dc2eb193605d6230286b5834c90eda801cc9
srozb <github@rozbicki.eu>
2022-10-11 14:01:15 +0200

    fix grep exports

--- src/peni/cmdGrep.nim
+++ src/peni/cmdGrep.nim
@@ -10,7 +10,7 @@ import ctx
 
 const COLOR = true
 
-proc matchImports(ctx: var pe_ctx_t, cPattern: Regex, ignoreCase: bool) =
+proc matchImports(ctx: var pe_ctx_t, cPattern: Regex) =
   for imp in pe_imports(addr ctx).items:
     for fun in imp.items:
       let qName = fmt"{imp.name}!{fun.name}"
@@ -19,19 +19,19 @@ proc matchImports(ctx: var pe_ctx_t, cPattern: Regex, ignoreCase: bool) =
         if COLOR: msg = msg.replace(cPattern, "$1".red)
         echo msg
 
-proc matchExports(ctx: var pe_ctx_t, cPattern: Regex, ignoreCase: bool) = 
+proc matchExports(ctx: var pe_ctx_t, cPattern: Regex) = 
   for exp in pe_exports(addr ctx).items:
     if ($exp.name).contains(cPattern):
       var msg = fmt"{ctx.path}," & fmt"{exp.address:#x}".blue & fmt":{exp.name} (export)"
       if COLOR: msg = msg.replace(cPattern, "$1".red)
+      echo msg
 
 proc grep*(ignoreCase = false, imports = false, exports = false, pattern = "", 
   recursive = false, files: seq[string]) =
   ## Search files of given criteria
   for c in files.peCtx(recursive=recursive):
     var ctx = c
-    var flags = ""
-    if ignoreCase: flags = "(?i)"
+    var flags = if ignoreCase: "(?i)" else: ""
     let cPattern = re(flags & "(" & pattern & ")")
-    if imports: ctx.matchImports(cPattern, ignoreCase)
-    if exports: ctx.matchExports(cPattern, ignoreCase)
+    if imports: ctx.matchImports(cPattern)
+    if exports: ctx.matchExports(cPattern)
